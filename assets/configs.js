62cbfa0fc802128b8218733b75b7bf823d4b0a65
thanhviet05x1d <34838579+thanhviet05x1d@users.noreply.github.com>
2021-01-28 22:30:33 +0700

    Add files via upload

---
+++ assets/configs.js
@@ -0,0 +1,52 @@
+const IMAGES = [
+  /*
+  {
+    name: 'Name of image',        // * Required:
+    image: 'template/test1.jpg',  // * Required:
+    width: 500,                   // * Required: Image width, pixel
+    height: 500,                  // * Required: Image height, pixel
+    position: [120, 300],         // * Required: [x, y]
+    align: 'center|left|right'    // * Required:
+    fontSize: 12,                 // Optional:
+    fontName: 'Arial',            // Optional:
+    color: '#FF00FF',             // Optional: Color code
+    rotate: 10,                   // Optional: Rotate text 10* clock wise
+  },
+  */
+  {
+    name: 'Bạn',
+    image: 'template/pic1.jpg',
+    width: 600,
+    height: 600,
+    position: [300, 400],
+    fontSize: 30,
+    color: '#FF0000',
+    rotate: 0,
+    fontName: 'Charmonman',
+    align: 'left'
+  },
+  {
+    name: 'Cô',
+    image: 'template/pic2.jpg',
+    width: 600,
+    height: 600,
+    position: [300, 400],
+    fontSize: 30,
+    color: '#FF0000',
+    rotate: 0,
+    fontName: 'Charmonman',
+    align: 'left'
+  },
+    {
+    name: 'Thầy',
+    image: 'template/pic3.jpg',
+    width: 600,
+    height: 600,
+    position: [300, 400],
+    fontSize: 30,
+    color: '#FF0000',
+    rotate: 0,
+    fontName: 'Charmonman',
+    align: 'left'
+  }
+]
